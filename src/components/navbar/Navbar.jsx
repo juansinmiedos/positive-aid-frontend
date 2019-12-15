0dcf29d68340bdb6f0cea17d82e2731df76cfc2d
Juan Vidal <juanvidaldom@gmail.com>
2019-12-14 23:55:37 -0600

    form added to the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,9 +6,11 @@ const Navbar = () => {
             <ul className="navbar">
                 <li className="logo">positive(+) aid</li>
                 <li>
-                    {/* <form action="">
-                        <input type="text"/>
-                    </form> */}
+                    <form action="">
+                        <input type="text" placeholder="Usuario" />
+                        <input type="password" placeholder="Contraseña"/>
+                        <button>Iniciar sesión</button>
+                    </form>
                 </li>
             </ul>
         </nav>
